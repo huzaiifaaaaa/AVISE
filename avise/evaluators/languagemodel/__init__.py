1c4aea0962519830ebd18589988daa8498b0e2d0
Yasamin Nikoukar <nkyasmin@yahoo.com>
2026-06-16 14:25:02 +0300

    Update __init__.py

--- avise/evaluators/languagemodel/__init__.py
+++ avise/evaluators/languagemodel/__init__.py
@@ -3,5 +3,6 @@ from .refusal import RefusalEvaluator
 from .vulnerability import VulnerabilityEvaluator
 from .partial_compliance import PartialComplianceEvaluator
 from .suspicious_output import SuspiciousOutputEvaluator
+from .semantic_compliance import SemanticComplianceEvaluator
 
 #__all__ = ["BaseLMEvaluator", "RefusalEvaluator", "VulnerabilityEvaluator", "PartialComplianceEvaluator", "SuspiciousOutputEvaluator"]
